fe399f36c500aaa7435134e04e038f5777eac7f1
kiko <kikocorreoso@gmail.com>
2018-04-12 11:07:24 +0200

    added code to pass tests

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -5,4 +5,19 @@ License: MIT
 """
 
 def suma(*args):
-    pass
+    """Sum indefinite number of integers and/or floats.
+    
+    Parameters
+    ----------
+    args : ints and/or floats
+        An indeterminate number of ints and/or floats.
+    
+    Returns
+    -------
+    value : int, float
+        Returns the result of the operation    
+    """
+    value = 0
+    for a in args:
+        value += a
+    return value
