4abda904a0286ebff77f48bca49c380f62a1762d
kiko <kikocorreoso@gmail.com>
2018-04-12 11:09:49 +0200

    added new tests

--- tests/test_mylib.py
+++ tests/test_mylib.py
@@ -4,6 +4,9 @@ from pathlib import Path
 # configure paths
 sys.path.append(str(Path('.')))
 
+import pytest
+from pytest import approx
+
 from mylib import suma
 
 def test_suma01():
@@ -11,3 +14,5 @@ def test_suma01():
     assert suma(0, 0, 0) == 0
     assert suma(-1, 1, -1, 1) == 0
     assert suma(1/4, 1/4, 1/4, 1/4) == 1
+    assert suma(1/10, 1/10, 1/10) == 0.3
+
