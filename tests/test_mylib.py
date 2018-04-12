be8a153e46915f52233e3b73f76c8d3a3f1a77a7
kiko <kikocorreoso@gmail.com>
2018-04-12 11:12:26 +0200

    more test passing

--- tests/test_mylib.py
+++ tests/test_mylib.py
@@ -14,5 +14,10 @@ def test_suma01():
     assert suma(0, 0, 0) == 0
     assert suma(-1, 1, -1, 1) == 0
     assert suma(1/4, 1/4, 1/4, 1/4) == 1
-    assert suma(1/10, 1/10, 1/10) == 0.3
+    assert suma(1/10, 1/10, 1/10) == approx(0.3)
 
+def test_suma02():
+    with pytest.raises(TypeError):
+        suma(1, '1')
+        suma(1, [1,2,3])
+        suma(1, (1,2,3))
